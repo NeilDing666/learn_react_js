b78422d1817d2fdf822600e869c6abe514571339
nd <zding501@gmail.com>
2020-07-19 13:48:00 +1000

    Initialize project using Create React App

--- src/index.js
+++ src/index.js
@@ -4,14 +4,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+let a = [10,20,30];
+//let ele = <div id='1' title='1'>{a}</div>
+ReactDOM.render(a.map(x=><li>{x}</li>),
+    document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+function showArray(arr) {
+    let i = [];
+    a.forEach(x=>(
+        i.push(<li>{x}</li>)
+    ))
+    return i;
+}
+
+ReactDOM.render(<div>{showArray(a)}</div>,
+    document.getElementById('root'))
